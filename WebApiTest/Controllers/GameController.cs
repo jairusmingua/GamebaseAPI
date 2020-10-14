2614707b687b293b95a3791f8a26d41630b33b4c
Jairus <minguajairus@yahoo.com.ph>
2020-10-14 14:31:35 +0800

    first commit with github

--- WebApiTest/Controllers/GameController.cs
+++ WebApiTest/Controllers/GameController.cs
@@ -11,12 +11,7 @@ namespace WebApiTest.Controllers
 {
     public class GameController : ApiController
     {
-        Game[] games = new Game[]
-        {
-            new Game{GameId=1,GameTitle="Call Of Duty: Ghosts",ImageUrl="https://vignette.wikia.nocookie.net/callofduty/images/6/6f/Call_of_Duty_Ghosts_PC_cover_art.jpg/revision/latest?cb=20130502141835",Developer="Infinity Ward",Synopsis="Sample Lang", MatureRating="PG", ReleaseDate=DateTime.Now},
-            new Game{GameId=2,GameTitle="Farcry Primal",ImageUrl="https://vignette.wikia.nocookie.net/callofduty/images/6/6f/Call_of_Duty_Ghosts_PC_cover_art.jpg/revision/latest?cb=20130502141835",Developer="Infinity Ward",Synopsis="Sample Lang", MatureRating="PG", ReleaseDate=DateTime.Now },
-            new Game{GameId=3,GameTitle="GTA Sa",ImageUrl="https://upload.wikimedia.org/wikipedia/en/c/c4/GTASABOX.jpg",Developer="Rockstar Games",Synopsis="Sample Lang", MatureRating="PG", ReleaseDate=DateTime.Now }
-        };  
+       
         [Route("api/game/topgames")]
         [HttpGet]
         public IEnumerable<Game>GetTopGames()
